b2c7d299dee01e1d59b86e97018416dcf09a80a4
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2020-06-10 12:34:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * Module dependencies.
+ */
+
+const app = require('express')();
+const nunjucks = require('nunjucks');
+const configVars = require('./config');
+
+const PORT = configVars.port || 5000
+
+app.set('view engine', 'html');
+
+nunjucks.configure(['views/'], {
+  autoescape: false,
+  express: app,
+});
+
+require('./routes')(app);
+
+app.listen(PORT, () => {
+  console.log('Running on port', PORT);
+});
