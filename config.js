852cf90a4973683be064af35794917484d5344c9
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2020-06-10 12:34:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+// Config Variables
+
+module.exports = {
+  baseUrlContentStack: 'https://cdn.contentstack.io/v3', //cdn baseurl
+  port: 4000,
+  apiKey: '', //your apikey  
+  accessToken: '', // delivery token 
+  env: '', //publishing environment
+  headerSection: {
+    headerContentTypeId: '', //header content-type uid
+  },
+  footerSection: {
+    footerContentTypeId: '', //footer content-type uid
+  },
+  homeSection: {
+    homeContentTypeId: '', //home content-type uid
+  },
+  expressBlogSection: {
+    blogContentTypeId: '', //blogs content-type uid
+  },
+};
