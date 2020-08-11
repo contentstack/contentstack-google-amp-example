30943f6839fa0c526717e1f7f304726570e7f67b
abhishek305 <abhishekshaji1994@gmail.com>
2020-08-11 15:18:54 +0530

    updates

--- middleware/partials.js
+++ middleware/partials.js
@@ -3,34 +3,37 @@
  */
 
 const async = require('async');
+const Contentstack = require('contentstack');
 const configVars = require('../config');
-const utils = require('../utils');
+
+// Initialize stack
+
+const Stack = Contentstack.Stack(configVars.apiKey, configVars.accessToken, configVars.env);
+
+// Below method will fetch header & footer data
 
 module.exports = (req, res, next) => {
   async.parallel(
     [
       // Here in this function we get the data for header
       (callback) => {
-        utils
-          .getData(
-            `${configVars.baseUrlContentStack}/content_types/${configVars.headerSection.headerContentTypeId}/entries?environment=${configVars.env}`,
-          )
-          .then((data) => {
-            callback(null, data.data);
+        const Query = Stack.ContentType(configVars.contentTypeUid.headerContentTypeUid).Query();
+        Query.toJSON()
+          .find()
+          .then((result) => {
+            callback(null, result[0][0]);
           })
           .catch((err) => {
             console.log(err);
           });
       },
       // Here in this function we get the data for footer
-
       (callback) => {
-        utils
-          .getData(
-            `${configVars.baseUrlContentStack}/content_types/${configVars.footerSection.footerContentTypeId}/entries?environment=${configVars.env}`,
-          )
-          .then((data) => {
-            callback(null, data.data);
+        const Query = Stack.ContentType(configVars.contentTypeUid.footerContentTypeUid).Query();
+        Query.toJSON()
+          .find()
+          .then((result) => {
+            callback(null, result[0][0]);
           })
           .catch((err) => {
             console.log(err);
