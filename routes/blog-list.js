ce75c68c7ad3952eb41282733a0b9005e6130d81
abhishek305 <abhishekshaji1994@gmail.com>
2020-08-11 15:55:19 +0530

    updates

---
+++ routes/blog-list.js
@@ -0,0 +1,32 @@
+/**
+ * Module dependencies.
+ */ 
+
+const express = require('express');
+
+const router = express.Router();
+const Contentstack = require('contentstack');
+const configVars = require('../config');
+
+// Initialize stack
+
+const Stack = Contentstack.Stack(configVars.apiKey, configVars.accessToken, configVars.env);
+
+// Below middleware will render the blog list page
+
+router.get('/', (req, res) => {
+  Stack.ContentType(configVars.contentTypeUid.blogContentTypeUid).Query()
+
+    .toJSON()
+    .find()
+    .then((result) => {
+      const blogList = result[0].find((blog) => blog.url === '/blogs');
+      res.render('pages/blog-list.html', { blogData: blogList });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+module.exports = router;
---
+++ routes/blog-page.js
@@ -0,0 +1,35 @@
+// Below code is the router method for an individual blog page`
+
+/**
+ * Module dependencies.
+ */
+
+const express = require('express');
+
+const router = express.Router();
+const Contentstack = require('contentstack');
+const configVars = require('../config');
+
+// Initialize stack
+
+const Stack = Contentstack.Stack(configVars.apiKey, configVars.accessToken, configVars.env);
+
+// Below router method will fetch & render a specific blog from modular block i.e blog_page
+
+router.get('/', (req, res) => {
+  Stack.ContentType(configVars.contentTypeUid.blogContentTypeUid).Query()
+
+    .toJSON()
+    .find()
+    .then((result) => {
+      const blogContent = result[0].find((blog) => `/blogs${blog.url}` === `${req.originalUrl}`);
+      if (blogContent) {
+        res.render('pages/blog-page.html', { blogDetail: blogContent });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
